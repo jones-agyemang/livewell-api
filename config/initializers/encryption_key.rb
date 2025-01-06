17249fc3014ae697d9434c78d62fe57a71837fbb
Jones Agyemang <mightyj@hotmail.co.uk>
2025-01-06 21:21:33 +0000

    Adds Encryption key configurations

---
+++ config/initializers/encryption_key.rb
@@ -0,0 +1,2 @@
+# Rails.application.config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA256
+# Rails.application.config.active_support.encryption_key_length = 32  # Important: 32 bytes for SHA256
